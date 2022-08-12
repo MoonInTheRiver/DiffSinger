00b624b7b638e9507d9017cf5a86ae6810cc65dc
oxygen-dioxide <54425948+oxygen-dioxide@users.noreply.github.com>
2022-08-12 11:12:35 +0800

    github-action

--- build.bat
+++ build.bat
@@ -2,3 +2,4 @@ WPy64-38100\python-3.8.10.amd64\python -m pip install "https://download.pytorch.
 WPy64-38100\python-3.8.10.amd64\python -m pip install https://download.pytorch.org/whl/torchaudio-0.6.0-cp38-none-win_amd64.whl
 WPy64-38100\python-3.8.10.amd64\python -m pip install "https://download.pytorch.org/whl/cpu/torchvision-0.7.0%%2Bcpu-cp38-cp38-win_amd64.whl"
 WPy64-38100\python-3.8.10.amd64\python -m pip install -r requirements_2080.txt
+WPy64-38100\python-3.8.10.amd64\python -m pip install -r requirements_server.txt
